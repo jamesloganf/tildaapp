505794433a2b396165ea75d85e720c7b37495c11
James <jamesloganforsyth@gmail.com>
2020-07-18 22:35:22 -0700

    Reorganized and added simple html template

--- run.py
+++ run.py
@@ -1,12 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hola():
-    return 'Hola mundo.'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from app import app
