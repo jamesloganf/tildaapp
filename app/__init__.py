68d4753487085d8373a93919245974d4c81e633c
James <jamesloganforsyth@gmail.com>
2020-10-04 16:45:12 -0700

    Added error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,5 +14,36 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+# Debugging
+if not app.debug:
+    # Creates email alerts for errors
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Tilda App Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # Creates log files for errors
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    # Limits log file size to 10KB and keeps the last 10 log files before files are overwritten
+    file_handler = RotatingFileHandler(
+        'logs/tildaapp.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.appHandler(file_handler)
+    app.logger.setlevel(logging.INFO)
+    app.logger.info('Tilda App startup')
+
+from app import routes, models, errors
 
